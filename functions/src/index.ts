82857036810b480789227e5cb0f7a7c6e0b14942
Brendan Goldsmith <brendangoldz@gmail.com>
2019-04-01 21:36:23 -0400

    Connected radius to user

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,35 @@
 import * as functions from 'firebase-functions';
-
+// import { environment } from '../environments/environment';
+// import * as firebase from 'firebase';
+import * as admin from 'firebase-admin';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+const serviceAccount = require("../environments/secret.json")
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://datbar-465c3.firebaseio.com"
 });
+
+
+export const dump_votes = functions.https.onRequest((message) => {
+    console.log("This job is run every day!", message);
+    const db = admin.firestore();
+    const bars = db.collection('bars').get().then((snapshot)=>{
+      snapshot.forEach((doc)=>{
+        console.log(doc.id, " => ", doc.data());
+        doc.data().votes = 0;
+      })
+    })
+    console.log("Bars Cleared", bars);
+    const users = db.collection('users').get().then((snapshot)=>{
+      snapshot.forEach((doc)=>{
+        console.log(doc.id, " => ", doc.data());
+        doc.data().voted = "";
+      })
+    })
+    console.log("Users Cleared", users);
+  });
